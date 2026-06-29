72f655f6984f370d35b579c284f46e01513b5270
Thomas <thomasweaverson@gmail.com>
2026-06-29 16:08:39 +0300

    module2-task1. Декомпозиция. Login Form, CityLink on login-page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,11 @@
+import CityLink from '../../components/city-link/city-link';
 import Header from '../../components/header/header';
+import LoginForm from '../../components/login-form/login-form';
+import { City } from '../../types/common';
 
 function Login(): JSX.Element {
+  //! mock
+  const mockCurrentCity: City = 'Amsterdam';
   return (
     <div className="page page--gray page--login">
       <Header />
@@ -9,24 +14,10 @@ function Login(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
-              </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
-            </form>
+            <LoginForm />
           </section>
           <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
-            </div>
+            <CityLink city={mockCurrentCity} />
           </section>
         </div>
       </main>
