7441615c7bbf4b4fe589e389dd02684f37b8dba2
thomasweaverson <thomasweaverson@gmail.com>
2026-08-14 13:35:18 +0300

    module9. e2e. Вернет название offers к названию из markup - properties

--- src/components/no-places/no-places.tsx
+++ src/components/no-places/no-places.tsx
@@ -8,7 +8,7 @@ const NoPlaces = (): JSX.Element => {
       <div className="cities__status-wrapper tabs__content">
         <b className="cities__status">No places to stay available</b>
         <p className="cities__status-description">
-          We could not find any offers available at the moment in{' '}
+          We could not find any property available at the moment in{' '}
           {currentCity.name}
         </p>
       </div>
